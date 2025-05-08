012607f9add9f84b2b4c06e155dc66201b64e2f4
wish <wish@sek.ai>
2025-05-08 23:53:32 +1000

    implement compile

--- jmp/jmp.go
+++ jmp/jmp.go
@@ -7,18 +7,25 @@ import (
 )
 
 type Jmp struct {
-	Jumps   []Jump
-	Menus   []Menu
-	Strings []string
+	Jumps      []Jump
+	Menus      []Menu
+	Strings    []string
+	ptrStrings []uint32
+
+	ptrJumps      uint32
+	ptrMenus      uint32
+	ptrPtrStrings uint32
 }
 
 type Jump struct {
-	Id           uint32
-	Unk          uint32            // icon or colour
-	StageIds     []uint16          // 4
-	Destinations []JumpDestination // 2
-	Title        string
-	Description  string
+	Id             uint32
+	Unk            uint32            // icon or colour
+	StageIds       []uint16          // 4
+	Destinations   []JumpDestination // 2
+	Title          string
+	Description    string
+	ptrTitle       uint32
+	ptrDescription uint32
 }
 
 type JumpDestination struct {
@@ -27,8 +34,10 @@ type JumpDestination struct {
 }
 
 type Menu struct {
-	Entries  []MenuEntry
-	StageIds []uint16
+	Entries     []MenuEntry
+	StageIds    []uint16
+	ptrEntries  uint32
+	ptrStageIds uint32
 }
 
 type MenuEntry struct {
@@ -126,4 +135,86 @@ func DecompileJmp(jmpData []byte) ([]byte, error) {
 	} else {
 		return jmpJson, nil
 	}
+}
+
+func CompileJmp(jmpJson []byte) ([]byte, error) {
+	jmp := Jmp{}
+	err := json.Unmarshal(jmpJson, &jmp)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	bf := byteframe.NewByteFrame()
+	bf.SetLE()
+	bf.WriteUint32(0)
+	bf.WriteUint32(0)
+	bf.WriteUint32(0)
+	bf.WriteUint32(0)
+	bf.WriteUint32(0)
+
+	for i := 0; i < len(jmp.Strings); i++ {
+		jmp.ptrStrings = append(jmp.ptrStrings, uint32(bf.Index()))
+		sjisString := sjis.NewString(jmp.Strings[i])
+		bf.WriteBytes(sjisString.Bytes())
+	}
+
+	for i := 0; i < len(jmp.Jumps); i++ {
+		jmp.Jumps[i].ptrTitle = uint32(bf.Index())
+		sjisTitle := sjis.NewString(jmp.Jumps[i].Title)
+		bf.WriteBytes(sjisTitle.Bytes())
+		jmp.Jumps[i].ptrDescription = uint32(bf.Index())
+		sjisDescription := sjis.NewString(jmp.Jumps[i].Description)
+		bf.WriteBytes(sjisDescription.Bytes())
+	}
+
+	for i := 0; i < len(jmp.Menus); i++ {
+		jmp.Menus[i].ptrEntries = uint32(bf.Index())
+		for j := 0; j < len(jmp.Menus[i].Entries); j++ {
+			bf.WriteUint16(jmp.Menus[i].Entries[j].Index)
+			bf.WriteUint16(jmp.Menus[i].Entries[j].Flags)
+		}
+		jmp.Menus[i].ptrStageIds = uint32(bf.Index())
+		for j := 0; j < len(jmp.Menus[i].StageIds); j++ {
+			bf.WriteUint16(jmp.Menus[i].StageIds[j])
+		}
+		bf.WriteUint16(0)
+	}
+
+	jmp.ptrPtrStrings = uint32(bf.Index())
+	for i := 0; i < len(jmp.ptrStrings); i++ {
+		bf.WriteUint32(jmp.ptrStrings[i])
+	}
+
+	jmp.ptrMenus = uint32(bf.Index())
+	for i := 0; i < len(jmp.Menus); i++ {
+		bf.WriteUint32(jmp.Menus[i].ptrEntries)
+		bf.WriteUint32(uint32(len(jmp.Menus[i].Entries)))
+		bf.WriteUint32(jmp.Menus[i].ptrStageIds)
+	}
+
+	jmp.ptrJumps = uint32(bf.Index())
+	for i := 0; i < len(jmp.Jumps); i++ {
+		bf.WriteUint32(jmp.Jumps[i].Id)
+		bf.WriteUint32(jmp.Jumps[i].Unk)
+		for j := 0; j < 4; j++ {
+			bf.WriteUint16(jmp.Jumps[i].StageIds[j])
+		}
+		for j := 0; j < 2; j++ {
+			for k := 0; k < 3; k++ {
+				bf.WriteFloat32(jmp.Jumps[i].Destinations[j].Coordinates[k])
+			}
+			bf.WriteUint32(jmp.Jumps[i].Destinations[j].Rotation)
+		}
+		bf.WriteUint32(jmp.Jumps[i].ptrTitle)
+		bf.WriteUint32(jmp.Jumps[i].ptrDescription)
+	}
+
+	_, _ = bf.Seek(0, 0)
+	bf.WriteUint32(jmp.ptrJumps)
+	bf.WriteUint32(jmp.ptrMenus)
+	bf.WriteUint32(uint32(len(jmp.Menus)))
+	bf.WriteUint32(jmp.ptrPtrStrings)
+	bf.WriteUint32(uint32(len(jmp.Strings)))
+
+	return bf.Data(), nil
 }
--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"hash/crc32"
 	"mhfrc/jmp"
 	"os"
 )
 
+const (
+	crcJmp = 0xB894E6DF
+)
+
 func main() {
-	fmt.Printf("Starting MHFReCompiler...")
-	decompile()
+	fmt.Printf("Starting MHFReCompiler...\n")
+	command := os.Args[1]
+	switch command {
+	case "decompile", "d":
+		decompile()
+	case "compile", "c":
+		compile()
+	default:
+		fmt.Printf("Invalid command. Use 'decompile' or 'compile'.\n")
+	}
 }
 
 func decompile() {
@@ -18,6 +31,13 @@ func decompile() {
 	if err != nil {
 		fmt.Printf("Error reading mhfjmp.bin: %v\n", err)
 	} else {
+		crc := crc32.ChecksumIEEE(binJmp)
+		if crc != crcJmp {
+			fmt.Printf("Detected non-vanilla mhfjmp.bin\n")
+		} else {
+			fmt.Printf("Detected vanilla mhfjmp.bin\n")
+		}
+
 		json, err := jmp.DecompileJmp(binJmp)
 		if err != nil {
 			fmt.Printf("Error decompiling mhfjmp.bin: %v\n", err)
@@ -27,6 +47,27 @@ func decompile() {
 				fmt.Printf("Error writing mhfjmp.json: %v\n", err)
 			} else {
 				fmt.Printf("Decompiled mhfjmp.bin to mhfjmp.json\n")
+			}
+		}
+	}
+}
+
+func compile() {
+	fmt.Printf("Compiling...\n")
+
+	jsonData, err := os.ReadFile("mhfjmp.json")
+	if err != nil {
+		fmt.Printf("Error reading mhfjmp.json: %v\n", err)
+	} else {
+		jmpData, err := jmp.CompileJmp(jsonData)
+		if err != nil {
+			fmt.Printf("Error compiling mhfjmp.json: %v\n", err)
+		} else {
+			err = os.WriteFile("mhfjmp_recomp.bin.", jmpData, 0644)
+			if err != nil {
+				fmt.Printf("Error writing mhfjmp_recomp.bin: %v\n", err)
+			} else {
+				fmt.Printf("Compiled mhfjmp.json to mhfjmp_recomp.bin\n")
 			}
 		}
 	}
